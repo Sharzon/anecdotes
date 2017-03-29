7b683b48ce05aa0d81e690cf2c473beb17db2ae4
Ilia Eliseev <sharzon.git@gmail.com>
2017-03-29 10:01:41 +0200

    Modificated Anecdote class

--- app/Anecdote.php
+++ app/Anecdote.php
@@ -4,58 +4,84 @@ namespace App;
 
 class Anecdote
 {
-    protected $type_filter = null;
-    protected $page_filter = null;
-    protected $accepting_filter = null;
+    protected static $pdo;
 
-    protected $pdo = PdoFactory::create();
+    public $id;
+    public $text;
+    public $type;
+    public $accepted;
+    public $got_at;
+    public $accepted_at;
 
-    public function filterByType($type)
-    {
-        $this->type_filter = $type;
-
-        return $this;
+    protected function __construct(
+        $id,
+        $text,
+        $type,
+        $accepted,
+        $got_at,
+        $accepted_at
+    ) {
+        $this->id = $id;
+        $this->text = $text;
+        $this->type = $type;
+        $this->accepted = $accepted;
+        $this->got_at = $got_at;
+        $this->accepted_at = $accepted_at;
     }
 
-    public function filterByPage($page)
-    {
-        $this->page_filter = $page;
-
-        return $this;
-    }
-    public function filterNonAccepted()
-    {
-        $this->accepting_filter = false;
-
-        return $this;
-    }
-
-    public function filterAccepted()
-    {
-        $this->accepting_filter = true;
-    }
-
-    public function getAll()
-    {
-
-    }
-
-    public function getById($id)
+    public static function getById($id)
     {
         $query = "SELECT * FROM anecdotes WHERE id = ?";
-        $statement = $this->pdo->prepare($query);
+
+        if (!self::$pdo)
+            self::makePdo();
+        $statement = self::$pdo->prepare($query);
         $statement->execute([$id]);
 
-        return $statement->fetch();
+        $db_anecdote = $statement->fetch();
+
+        $anecdote = new Anecdote(
+            $db_anecdote->id,
+            $db_anecdote->text,
+            $db_anecdote->type_id,
+            $db_anecdote->accepted,
+            $db_anecdote->got_at,
+            $db_anecdote->accepted_at
+        );
+
+        return $anecdote;
     }
-    
+
     public function accept()
     {
-        
+        $query = "UPDATE anecdotes 
+                  SET accepted = 1,
+                      accepted_at = NOW() 
+                  WHERE id = ?";
+
+        if (!self::$pdo)
+            self::makePdo();
+        $statement = self::$pdo->prepare($query);
+        $statement->execute([$this->id]);
+
+        $anecdote = self::getById($this->id);
+
+        $this->id = $anecdote->id;
+        $this->text = $anecdote->text;
+        $this->type = $anecdote->type;
+        $this->accepted = $anecdote->accepted;
+        $this->got_at = $anecdote->got_at;
+        $this->accepted_at = $anecdote->accepted_at;
     }
 
     public function notAccept()
     {
+        $query = "DELETE FROM anecdotes
+                  WHERE id = ?";
+    }
 
+    protected static function makePdo()
+    {
+        self::$pdo = PdoFactory::create();
     }
 }
